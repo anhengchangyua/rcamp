3bb24112ebae5c9346c7f965b7f9da79172c5c3c
anhengchangyua <188851312@qq.com>
2018-09-30 14:13:12 +0800

    banner component

--- app/containers/MainContainer.js
+++ app/containers/MainContainer.js
@@ -1,16 +1,178 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { Text } from 'react-native';
+import {
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  Alert,
+  AppRegistry,
+  StyleSheet,
+  ListView,
+  View,
+  Dimensions
+} from 'react-native';
+import Swiper from 'react-native-swiper';
+
+const { width, height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  wrapper: {},
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB'
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5'
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
+  },
+  slide4: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
 class MainContainer extends React.Component {
-  static navigationOptions = {
-    title: '首页',
-    tabBarIcon: ({ tintColor }) => (
-      <Icon name="md-home" size={25} color={tintColor} />
-    )
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: '首页',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="md-home" size={25} color={tintColor} />
+      ),
+      headerRight: (
+        <TouchableWithoutFeedback onPress={navigation.getParam('imageOnclick')}>
+          <Image
+            style={{ width: 38, height: 38, marginRight: 12 }}
+            source={require('../assets/search.png')}
+          />
+        </TouchableWithoutFeedback>
+      )
+    };
   };
 
+  componentWillMount() {
+    this.props.navigation.setParams({ imageOnclick: this._imageOnclick });
+  }
+
+  _imageOnclick = () => {
+    Alert.alert('onPressButton');
+  };
   render() {
-    return <Text>这是首页</Text>;
+    return (
+      <View
+        style={{
+          width: width,
+          height: 160,
+          backgroundColor: '#fff'
+        }}
+      >
+        <Swiper
+          style={styles.wrapper}
+          height={240}
+          onMomentumScrollEnd={(e, state, context) =>
+            console.log('index:', state.index)
+          }
+          dot={
+            <View
+              style={{
+                backgroundColor: 'rgba(0,0,0,.2)',
+                width: 5,
+                height: 5,
+                borderRadius: 4,
+                marginLeft: 3,
+                marginRight: 3,
+                marginTop: 3,
+                marginBottom: 3
+              }}
+            />
+          }
+          activeDot={
+            <View
+              style={{
+                backgroundColor: '#000',
+                width: 8,
+                height: 8,
+                borderRadius: 4,
+                marginLeft: 3,
+                marginRight: 3,
+                marginTop: 3,
+                marginBottom: 3
+              }}
+            />
+          }
+          paginationStyle={{
+            bottom: -23,
+            left: null,
+            right: 10
+          }}
+          loop
+        >
+          <View
+            style={styles.slide1}
+            title={
+              <Text numberOfLines={1}>Aussie tourist dies at Bali hotel</Text>
+            }
+          >
+            <Image
+              style={{ width: width, height: 160 }}
+              resizeMode="stretch"
+              source={require('../assets/img/1.jpg')}
+            />
+          </View>
+          <View
+            style={styles.slide2}
+            title={
+              <Text numberOfLines={1}>Aussie tourist dies at Bali hotel</Text>
+            }
+          >
+            <Image
+              style={{ width: width, height: 160 }}
+              resizeMode="stretch"
+              source={require('../assets/img/2.jpg')}
+            />
+          </View>
+          <View
+            style={styles.slide3}
+            title={
+              <Text numberOfLines={1}>Aussie tourist dies at Bali hotel</Text>
+            }
+          >
+            <Image
+              style={{ width: width, height: 160 }}
+              resizeMode="stretch"
+              source={require('../assets/img/3.jpg')}
+            />
+          </View>
+          <View
+            style={styles.slide4}
+            title={
+              <Text numberOfLines={1}>Aussie tourist dies at Bali hotel</Text>
+            }
+          >
+            <Image
+              style={{ width: width, height: 160 }}
+              resizeMode="stretch"
+              source={require('../assets/img/4.jpg')}
+            />
+          </View>
+        </Swiper>
+      </View>
+    );
   }
 }
 
